3a217d0d78e7c846f070e3e6be42e020b6e23b48
Arnab Saha <arnabsahawrk@gmail.com>
2026-01-18 18:50:34 +0600

    project setup

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "rest_framework",
     "api",
     "accounts",
     "library",
@@ -118,3 +119,15 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = "static/"
+
+if DEBUG:
+    INSTALLED_APPS += ["debug_toolbar"]
+
+    MIDDLEWARE = [
+        "debug_toolbar.middleware.DebugToolbarMiddleware",
+    ] + MIDDLEWARE
+
+
+INTERNAL_IPS = ["127.0.0.1"]
+
+REST_FRAMEWORK = {}
