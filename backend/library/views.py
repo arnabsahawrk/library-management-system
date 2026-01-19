c7824366307006a5291ea8e1fc1861621d1a9148
Arnab Saha <arnabsahawrk@gmail.com>
2026-01-19 21:28:47 +0600

    add: create serializers for library app and define all api

--- backend/library/views.py
+++ backend/library/views.py
@@ -1,3 +1,59 @@
-from django.shortcuts import render
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework import viewsets
 
-# Create your views here.
+from .filters import (
+    AuthorFilter,
+    BookFilter,
+    BorrowRecordFilter,
+    CategoryFilter,
+    MemberFilter,
+)
+from .models import Author, Book, BorrowRecord, Category, Member
+from .paginations import DefaultPagination
+from .serializers import (
+    AuthorSerializer,
+    BookSerializer,
+    BorrowRecordSerializer,
+    CategorySerializer,
+    MemberSerializer,
+)
+
+
+class AuthorViewSet(viewsets.ModelViewSet):
+    queryset = Author.objects.all()
+    serializer_class = AuthorSerializer
+    filter_backends = [DjangoFilterBackend]
+    filterset_class = AuthorFilter
+    pagination_class = DefaultPagination
+
+
+class CategoryViewSet(viewsets.ModelViewSet):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+    filter_backends = [DjangoFilterBackend]
+    filterset_class = CategoryFilter
+    pagination_class = DefaultPagination
+
+
+class BookViewSet(viewsets.ModelViewSet):
+    queryset = Book.objects.select_related("category").prefetch_related("author")
+    serializer_class = BookSerializer
+    filter_backends = [DjangoFilterBackend]
+    filterset_class = BookFilter
+    pagination_class = DefaultPagination
+
+
+class MemberViewSet(viewsets.ModelViewSet):
+    queryset = Member.objects.all()
+    serializer_class = MemberSerializer
+    filter_backends = [DjangoFilterBackend]
+    filterset_class = MemberFilter
+    pagination_class = DefaultPagination
+
+
+class BorrowRecordViewSet(viewsets.ModelViewSet):
+    queryset = BorrowRecord.objects.select_related("book", "member")
+    serializer_class = BorrowRecordSerializer
+    filter_backends = [DjangoFilterBackend]
+    filterset_class = BorrowRecordFilter
+    pagination_class = DefaultPagination
